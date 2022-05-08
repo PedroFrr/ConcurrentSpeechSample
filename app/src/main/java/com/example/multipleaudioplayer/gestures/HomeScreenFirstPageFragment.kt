f13292cf7c6a61a4a35a396f60078504e1a9d2e8
Pedro Francisco <>
2022-05-08 22:36:20 +0100

    Add different elements sounds

--- app/src/main/java/com/example/multipleaudioplayer/gestures/HomeScreenFirstPageFragment.kt
+++ app/src/main/java/com/example/multipleaudioplayer/gestures/HomeScreenFirstPageFragment.kt
@@ -1,7 +1,11 @@
 package com.example.multipleaudioplayer.gestures
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.multipleaudioplayer.R
@@ -15,19 +19,74 @@ class HomeScreenFirstPageFragment : Fragment(R.layout.layout_homescreen_first_pa
 
     private val listAdapter by lazy { ItemsListAdapter() }
 
+    private val imageViewMediaPlayer: MediaPlayer by lazy {
+        MediaPlayer.create(context, R.raw.image_view)
+    }
+
+    private val textViewMediaPlayer: MediaPlayer by lazy {
+        MediaPlayer.create(context, R.raw.text_view)
+    }
+
+    private val editTextMediaPlayer: MediaPlayer by lazy {
+        MediaPlayer.create(context, R.raw.edit_text)
+    }
+
+    private val switchMediaPlayer: MediaPlayer by lazy {
+        MediaPlayer.create(context, R.raw.switch_sound)
+    }
+
+    private val radioButtonMediaPlayer: MediaPlayer by lazy {
+        MediaPlayer.create(context, R.raw.radio_button)
+    }
+
+    private val checkboxMediaPlayer: MediaPlayer by lazy {
+        MediaPlayer.create(context, R.raw.check_box)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         setupUi()
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     private fun setupUi() {
         setupListAdapter()
 
         listAdapter.submitList(icons)
+
+        binding.clContainer.setOnTouchListener { view, _ ->
+            val imm = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view?.windowToken, 0)
+            true
+        }
+
+        binding.apply {
+            cb.setOnClickListener {
+                checkboxMediaPlayer.start()
+            }
+            et.setOnClickListener {
+                editTextMediaPlayer.start()
+            }
+            iv.setOnClickListener {
+                imageViewMediaPlayer.start()
+            }
+            rbOption1.setOnClickListener {
+                radioButtonMediaPlayer.start()
+            }
+            rbOption2.setOnClickListener {
+                radioButtonMediaPlayer.start()
+            }
+            sv.setOnClickListener {
+                switchMediaPlayer.start()
+            }
+            tv.setOnClickListener {
+                textViewMediaPlayer.start()
+            }
+        }
     }
 
-    private fun setupListAdapter(){
+    private fun setupListAdapter() {
         binding.rvHomescreen.apply {
             adapter = listAdapter
             layoutManager = GridLayoutManager(requireContext(), 3)
